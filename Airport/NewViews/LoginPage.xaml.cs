87df78e2184c588f90f71c731a35d51340d430d9
Георгий Рогава <gmrogava@edu.hse.ru>
2016-03-20 19:23:10 +0300

    UI update

--- Airport/NewViews/LoginPage.xaml.cs
+++ Airport/NewViews/LoginPage.xaml.cs
@@ -26,5 +26,10 @@ namespace Airport.NewViews
         {
             this.InitializeComponent();
         }
+
+        private void TextBox_TextChanged(System.Object sender, TextChangedEventArgs e)
+        {
+
+        }
     }
 }
